6811de1a742d5cf5dd7a6e7894471d2c1e35eb57
JIan Loong Liew <jianloongliew@gmail.com>
2016-02-02 00:04:10 +1100

    Added stock maximise solution

---
+++ src/BreadthFirstSearch.java
@@ -0,0 +1,12 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+/**
+ * Created by Jian on 1/02/2016.
+ */
+public class BreadthFirstSearch {
+    
+}
+
