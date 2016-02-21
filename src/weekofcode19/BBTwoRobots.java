36430298474d7cf64905eea7c16c682c900a42c2
JIan Loong Liew <jianloongliew@gmail.com>
2016-02-22 04:45:50 +1100

    Two Robots P1

--- src/weekofcode19/BBTwoRobots.java
+++ src/weekofcode19/BBTwoRobots.java
@@ -2,9 +2,8 @@ package weekofcode19;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 /**
@@ -14,12 +13,12 @@ import java.util.Scanner;
  */
 public class BBTwoRobots {
 
-
     public static void main(String[] args) throws FileNotFoundException {
         Scanner sc = new Scanner(new FileInputStream("testcase.txt"));
         int t = sc.nextInt();
-
         for (int test = 0; test < t; test++) {
+
+            int minDistance = Integer.MAX_VALUE;
 
             int noOfContainers = sc.nextInt();
             int queries = sc.nextInt();
@@ -31,97 +30,77 @@ public class BBTwoRobots {
                 mB[i] = sc.nextInt();
             }
 
-            int minDistance = Integer.MAX_VALUE;
-
-            HashMap<Integer, ArrayList<Node>> potentialNodes = new HashMap<Integer, ArrayList<Node>>();
-
-            Node root = new Node();
-            root.robotNo = 1;
-            root.parent = null;
-            root.boundingFunction = Math.abs(mA[0] - mB[0]);
-            root.level = 0;
-
-            ArrayList<Node> al = new ArrayList<>();
-            al.add(root);
-            potentialNodes.put(root.boundingFunction, al);
-
+            //HashMap<Integer, ArrayList<Node>> potentialNodes = new HashMap<Integer, ArrayList<Node>>();
+            LinkedList<Node> list = new LinkedList<>();
+            int dist = Math.abs(mA[0] - mB[0]);
+            Node root = new Node(1, dist, 0, null);
+            //ArrayList<Node> rootArray = new ArrayList<>();
+            //rootArray.add(root);
+            //potentialNodes.put(root.boundingFunction, rootArray);
+            list.add(root);
             for (; ; ) {
 
-                if (potentialNodes.size() == 0)
+                //if (potentialNodes.size() == 0)
+                //    break;
+                //int min = Collections.min(potentialNodes.keySet());
+                //ArrayList<Node> listNode = potentialNodes.get(min);
+                //if (listNode.size() == 0)
+                //break;
+                Node node = list.pollLast();
+                if(node == null)
                     break;
-
-                int min = Collections.min(potentialNodes.keySet());
-                ArrayList<Node> listNode = potentialNodes.get(min);
-
-                Node node = listNode.get(0);
-                listNode.remove(0);
-
-                if (listNode.size() == 0)
-                    potentialNodes.remove(min);
-                else
-                    potentialNodes.put(min, listNode);
-
-                if (node.level == mA.length - 1) {
-                    System.out.println(node);
-                    System.out.println(print(node));
-                    System.out.println(node.boundingFunction);
-                    break;
+                //Node node = listNode.get(0);
+                //listNode.remove(0);
+                //if (listNode.size() == 0)
+                //   potentialNodes.remove(min);
+                //else
+                //    potentialNodes.put(min, listNode);
+                if (node.level == queries - 1) {
+                    //System.out.println(node.boundingFunction);
+                    minDistance = Math.min(node.boundingFunction, minDistance);
+                    LinkedList<Integer> result = getRobot(node, new LinkedList<>());
+                    Collections.reverse(result);
+                    System.out.println(result + " " + node.boundingFunction);
                 }
-
-
-                if (node.level + 1 < queries) {
-                    for (int robot = 1; robot <= 2; robot++) {
-                        Node n = new Node();
-                        n.robotNo = robot;
-                        n.parent = node;
-                        n.level = node.level + 1;
-
-                        int value = 0;
-                        Node previousNode = find(robot, node);
-
-                        if (previousNode == null) {
-                            value = 0;
-                        } else {
-                            value = Math.abs(mB[previousNode.level] - mA[n.level]);
-                        }
-
-                        int queryDistance = Math.abs(mA[n.level] - mB[n.level]);
-
-                        n.boundingFunction = value + queryDistance + node.boundingFunction;
-
-                        if (potentialNodes.containsKey(n.boundingFunction)) {
-                            ArrayList<Node> list = potentialNodes.get(n.boundingFunction);
-                            list.add(n);
-                            potentialNodes.put(n.boundingFunction, list);
-                        } else {
-                            ArrayList<Node> list = new ArrayList<>();
-                            list.add(n);
-                            potentialNodes.put(n.boundingFunction, list);
-                        }
-                    }
+                for (int robot = 1; robot <= 2; robot++) {
+                    int pos = getParent(node, robot);
+                    if (node.level == queries - 1)
+                        break;
+                    int value = 0;
+                    //System.out.println(pos);
+                    if (pos != -1)
+                        value = Math.abs(mB[pos] - mA[node.level + 1]);
+                    else
+                        value = 0;
+                    int boundingFunction = value + node.boundingFunction + Math.abs(mA[node.level + 1] - mB[node.level + 1]);
+                    Node child = new Node(robot, boundingFunction, node.level + 1, node);
+                    //ArrayList<Node> arrayList = potentialNodes.getOrDefault(boundingFunction, new ArrayList<>());
+                    //arrayList.add(child);
+                    //potentialNodes.put(boundingFunction, arrayList);
+                    list.add(child);
                 }
             }
+            System.out.println(minDistance);
         }
     }
-
-    public static String print(Node node) {
+    public static LinkedList getRobot(Node node, LinkedList<Integer> list) {
+        list.add(node.robotNo);
         if (node.parent == null)
-            return "";
-        System.out.println(node.parent);
-        print(node.parent);
-        return "";
+            return list;
+        getRobot(node.parent, list);
+        return list;
     }
 
-    public static Node find(int robot, Node node) {
-        if (node.robotNo == robot) {
-            return node;
-        } else {
-            if (node.parent == null)
-                return null;
-            find(robot, node.parent);
+    public static int getParent(Node node, int robotNo) {
+        LinkedList<Integer> al = getRobot(node, new LinkedList<>());
+        //System.out.println(al + " search for" + " " + robotNo);
+        for (int i = al.size() - 1; i >= 0; i--) {
+            if (al.get(i) == robotNo)
+                return i;
         }
-        return null;
+        return -1;
     }
+
 
     public static class Node {
         public int robotNo;
@@ -129,9 +108,16 @@ public class BBTwoRobots {
         public int level;
         public Node parent;
 
-        public String toString() {
-            return "Level : " + level + " R : " + robotNo + " BF: " + boundingFunction + "||";
+
+        public Node(int robotNo, int boundingFunction, int level, Node parent) {
+            this.robotNo = robotNo;
+            this.boundingFunction = boundingFunction;
+            this.level = level;
+            this.parent = parent;
         }
 
+        public String toString() {
+            return "BF : " + boundingFunction + " LE : " + level + " || ";
+        }
     }
 }
